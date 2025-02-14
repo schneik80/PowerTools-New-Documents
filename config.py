7c962b217f0e70d4d781d442283a90ebd5e9543d
Kevin Schneider <schneik80@gmail.com>
2025-02-13 19:40:56 -0800

    edit config

--- config.py
+++ config.py
@@ -4,16 +4,10 @@ import os.path
 import json
 from .lib import fusionAddInUtils as futil
 
-# Global Variables
 DEBUG = True
-# ADDIN_NAME is derived from the directory name of the current file
-ADDIN_NAME = os.path.basename(os.path.dirname(__file__))
 ADDIN_NAME = os.path.basename(os.path.dirname(__file__))
 COMPANY_NAME = "IMA LLC"
 
-# Unique identifier for the sample palette
-sample_palette_id = f"{COMPANY_NAME}_{ADDIN_NAME}_palette_id"
-sample_palette_id = f"{COMPANY_NAME}_{ADDIN_NAME}_palette_id"
 
 design_workspace = "FusionSolidEnvironment"
 tools_tab_id = "ToolsTab"
