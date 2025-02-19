80a0721ecfe7867bd26bfe23393ce2b7c1636a67
Kevin Schneider <schneik80@gmail.com>
2025-02-18 16:32:54 -0800

    move commands

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,30 +1,16 @@
-from .assemblystats import entry as assemblystats
 from .autosave import entry as autosave
 from .datatoggle import entry as datatoggle
 from .dochistory import entry as dochistory
 from .docinfo import entry as docinfo
-from .getandupdate import entry as getandupdate
-from .insertSTEP import entry as insertSTEP
-from .makeassemblyfrom import entry as makeassemblyfrom
 from .new import entry as new
-from .refmanager import entry as refmanager
-from .refrences import entry as refrences
-from .refresh import entry as refresh
 
 # Fusion will automatically call the start() and stop() functions.
 commands = [
-    assemblystats,
     autosave,
     datatoggle,
     dochistory,
     docinfo,
-    getandupdate,
-    insertSTEP,
-    makeassemblyfrom,
     new,
-    refmanager,
-    refrences,
-    refresh,
 ]
 
 
