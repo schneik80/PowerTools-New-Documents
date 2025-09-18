b76ed3f540fc6554bccf5fe7d0332b6d7ede801a
Kevin Schneider <schneik80@gmail.com>
2025-09-18 10:07:35 -0500

    Changes for insider intent based documents

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,19 +1,15 @@
 from .autosave import entry as autosave
-from .changetype import entry as changetype
 from .datatoggle import entry as datatoggle
 from .dochistory import entry as dochistory
 from .docinfo import entry as docinfo
-from .makeassemblyfrom import entry as makeassemblyfrom
 from .new import entry as new
 
 # Fusion will automatically call the start() and stop() functions.
 commands = [
     autosave,
     datatoggle,
-    changetype,
     dochistory,
     docinfo,
-    makeassemblyfrom,
     new,
 ]
 
